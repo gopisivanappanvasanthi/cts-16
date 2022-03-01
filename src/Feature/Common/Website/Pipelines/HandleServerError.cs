cbeabe02d2a75bff7ce4f017850f44588f8da172
Gopi <gopi.sv@verticurl.com>
2022-03-01 11:52:52 +0530

    Pipelines

---
+++ src/Feature/Common/Website/Pipelines/HandleServerError.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Pipelines.HttpRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cts.Feature.Common.Pipelines
+{
+    public class HandleServerError : Sitecore.Pipelines.HttpRequest.HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            if(HttpContext.Current.Response.StatusCode == 500)
+            {
+                var pageNotFoundItem = Sitecore.Context.Database.GetItem(new Sitecore.Data.ID("{494331D1-6DC0-47B9-A1D7-FDF6640E7AA8}"));
+                Sitecore.Context.Item = pageNotFoundItem;
+            }
+        }
+    }
+}
---
+++ src/Feature/Common/Website/Pipelines/PageNotFoundResolver.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Pipelines.HttpRequest;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Cts.Feature.Common.Pipelines
+{
+    public class PageNotFoundResolver : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            //args.Url.FilePath
+            if (args.Url.FilePath.Contains("/sitecore") || File.Exists(args.Url.FilePath))
+                return;
+
+            var contextItem = Sitecore.Context.Item;
+
+            if(contextItem is null)
+            {
+                var pageNotFoundItem = Sitecore.Context.Database.GetItem(new Sitecore.Data.ID("{494331D1-6DC0-47B9-A1D7-FDF6640E7AA8}"));
+                Sitecore.Context.Item = pageNotFoundItem;
+            }
+
+        }
+    }
+}
