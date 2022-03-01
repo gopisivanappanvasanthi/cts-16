faa3798ab55c59aaf1f0b51bb81f97dc3cbed18d
Gopi <gopi.sv@verticurl.com>
2022-03-01 07:36:00 +0530

    Sitecore

---
+++ src/Project/Cts/Website/Example/MultilistResolver.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore.LayoutService.ItemRendering.ContentsResolvers;
+using Sitecore.LayoutService.Configuration;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data;
+using Newtonsoft.Json.Linq;
+
+namespace Cts.Project.Cts.Example
+{
+    public class MultilistResolver : RenderingContentsResolver
+    {
+        public override object ResolveContents(Rendering rendering, IRenderingConfiguration renderingConfig)
+        {
+            Assert.ArgumentNotNull(rendering, nameof(rendering));
+            Assert.ArgumentNotNull(renderingConfig, nameof(renderingConfig));
+
+            Item ds = GetContextItem(rendering, renderingConfig);
+
+            var multilistitemfieldid = new ID("");
+
+            MultilistField multilistField = ds.Fields[multilistitemfieldid];
+
+            JObject jobject = new JObject
+            {
+                ["multilistitems"] = (JToken)new JArray()
+            };
+            jobject["multilistitems"] = ProcessItems(multilistField.GetItems(), rendering, renderingConfig);
+
+            return jobject;
+        }
+    }
+}
---
+++ src/Project/Cts/Website/Example/SecondMultilistResolver.cs
@@ -0,0 +1,61 @@
+﻿using Sitecore.LayoutService.ItemRendering.ContentsResolvers;
+using Sitecore.LayoutService.Configuration;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data;
+using Newtonsoft.Json.Linq;
+using Sitecore.Globalization;
+
+namespace Cts.Project.Cts.Example
+{
+    public class SecondMultilistResolver : RenderingContentsResolver
+    {
+        public override object ResolveContents(Rendering rendering, IRenderingConfiguration renderingConfig)
+        {
+
+            Assert.ArgumentNotNull(rendering, nameof(rendering));
+            Assert.ArgumentNotNull(renderingConfig, nameof(renderingConfig));
+
+            Item ds = GetContextItem(rendering, renderingConfig);
+
+            var multilistitemfieldid = new ID("");
+
+            MultilistField multilistField = ds.Fields[multilistitemfieldid];
+
+            JObject jobject = new JObject
+            {
+                ["multilistitems"] = (JToken)new JArray()
+            };
+
+            var createdcustomitem = multilistField.GetItems()
+                                            .Select(x => MapItemToRenderingModelItem(x))
+                                            .ToList();
+
+            jobject["multilistitems"] = ProcessItems(createdcustomitem, rendering, renderingConfig);
+
+            return jobject;
+
+        }
+
+        private Item MapItemToRenderingModelItem(Item item)
+        {
+            var newId = new ID();
+            var def = new ItemDefinition(newId, item.Name, new ID(""), ID.Null);
+            var fields = new FieldList();
+            fields.Add(new ID(""), "");
+            fields.Add(new ID(""), "");
+            fields.Add(new ID(""), "");
+
+            var data = new ItemData(def, Language.Current, Sitecore.Data.Version.First, fields);
+            var db = Sitecore.Context.Database;
+            var resultItem = new Item(newId, data, db);
+            return resultItem;
+        }
+    }
+}
